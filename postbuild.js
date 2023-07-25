22d5ea7818f9ca19fbad9b5ec132a78fa7a65864
Armando M <cuentafrst@gmail.com>
2023-07-25 15:29:35 -0600

    -

--- postbuild.js
+++ postbuild.js
@@ -1,14 +1,14 @@
 import { copyFile, readdirSync } from "fs";
-import { join } from "path";
+import { join, resolve } from "path";
 
-const sourceFolder = "./src/lib";
-const destinationFolder = "./dist/lib";
+const sourceFolder = "src/lib";
+const destinationFolder = "dist/lib";
 const tsFile = /^(?!.*\.d\.ts$).*\.ts$/;
 
-readdirSync(sourceFolder).forEach((filePath) => {
+readdirSync(resolve(sourceFolder)).forEach((filePath) => {
   if (!tsFile.test(filePath)) {
-    const source = join(sourceFolder, filePath);
-    const destination = join(destinationFolder, filePath);
+    const source = resolve(join(sourceFolder, filePath));
+    const destination = resolve(join(destinationFolder, filePath));
     copyFile(source, destination, (err) => {
       if (err) {
         console.error("Failed to copy", filePath);

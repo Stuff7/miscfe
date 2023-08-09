3334f38ececfdd2c13777eb479278868e4081edc
Armando <cuentafrst@gmail.com>
2023-08-09 06:01:31 +0000

    Build static site.

--- src/app/main.ts
+++
@@ -1,7 +0,0 @@
-import "@app/style/main.scss";
-import { createApp } from "vue";
-import App from "@app/App.vue";
-import { setLocalNamespace } from "@lib/storage";
-
-setLocalNamespace("TEST_APP");
-createApp(App).mount("#app");
